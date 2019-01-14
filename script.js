c711cd912ba510fad7c549fdb31990ff3609e7ba
jorn55 <jorn55@gmail.com>
2019-01-14 10:36:54 +0200

    First commit - mostly heading

---
+++ script.js
@@ -0,0 +1,25 @@
+// function getValEmail() {
+// 	var email1 = document.getElementById("email").value;
+// 	console.log("email = " + email1);
+// }
+
+// function sendVals() {
+// 	var name1 = document.getElementById("name").value;
+// 	var email1 = document.getElementById("email").value;
+// 	var passw1 = document.getElementById("passw1").value;
+// 	var passw2 = document.getElementById("passw2").value;
+// 	console.log(name1 + " " + email1 + " " +  passw1);
+
+// }
+
+// function comparePwd() {
+// 	var pwd1 = document.getElementById("passw1").value;
+// 	var pwd2 = document.getElementById("passw2").value;
+// 	if ( pwd1 == pwd2) {
+// 		document.getElementById("paswdNo").innerHTML = "Match!";
+// 		document.getElementById("paswdNo").style.color = "green";
+// 	} else {
+// 		document.getElementById("paswdNo").innerHTML = "No match.";
+// 		document.getElementById("paswdNo").style.color = "red";
+// 	}
+// }
