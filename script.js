531e1cc815c512f0e1e075c60aa426361912d4dc
jorn55 <jorn55@gmail.com>
2019-01-17 09:55:42 +0200

    more fixes

--- script.js
+++ script.js
@@ -19,18 +19,18 @@ var myVar2 = setInterval(changePictures2, 5000);
 // }
 
 function changePictures1() {
-    if ( document.getElementById("img1").src == "images/250X110668568400.jpg" ) {
-        document.getElementById("img1").src = "images/banner_atar.jpg";
+    if ( document.getElementById("img1").src == "../images/250X110668568400.jpg" ) {
+        document.getElementById("img1").src = "../images/banner_atar.jpg";
     } else {
-        document.getElementById("img1").src = "images/250X110668568400.jpg";
+        document.getElementById("img1").src = "../images/250X110668568400.jpg";
     }
 }
 
 function changePictures2() {
-    if ( document.getElementById("img2").src == "images/paz_250x110059890678.jpg" ) {
-        document.getElementById("img2").src = "images/paz_250x110103131473.jpg";
+    if ( document.getElementById("img2").src == "../images/paz_250x110059890678.jpg" ) {
+        document.getElementById("img2").src = "../images/paz_250x110103131473.jpg";
     } else {
-        document.getElementById("img2").src = "images/paz_250x110059890678.jpg";
+        document.getElementById("img2").src = "../images/paz_250x110059890678.jpg";
     }
 }
 
